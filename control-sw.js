dcb1ef978376522c92b3088ba02ac73d1d2b98e4
u436350 <u4363505921@gmail.com>
2026-04-17 09:31:53 +0200

    style(login): improve compact language field with flag dropdown

--- control-sw.js
+++ control-sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "baupass-control-shell-v5";
-const RUNTIME_CACHE = "baupass-control-runtime-v5";
+const SHELL_CACHE = "baupass-control-shell-v6";
+const RUNTIME_CACHE = "baupass-control-runtime-v6";
 const SHELL_ASSETS = [
   "/",
   "/index.html",
